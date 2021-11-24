56c2f3a65c88234581e36accee8fd26d181259a5
Sharan AJ <sharanajs2001@gmail.com>
2021-11-24 16:18:43 +0530

    tailwind css added and configured

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from "react";
+
 function App() {
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+    <div className="h-screen w-screen">
+      <h1 className="font-mono text-3xl text-center text-blue-600 font-semibold">{"hello world".toUpperCase()}</h1>
+    </div>
   );
 }
 
