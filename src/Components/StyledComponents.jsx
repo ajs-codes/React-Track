c855cf239192d3dce9ff318f11fa48f7bdaafe6a
Sharan AJ <sharanajs2001@gmail.com>
2021-12-30 11:13:55 +0530

    styled setup 2

--- src/Components/StyledComponents.jsx
+++ src/Components/StyledComponents.jsx
@@ -1,11 +1,25 @@
+import styled from "styled-components";
+
 const StyledComponents = () => {
   return (
     <main>
       <header>
         <h1>Styled Components tutorial</h1>
+        <Custombtn>Submit</Custombtn>
       </header>
     </main>
   );
 };
+
+const Custombtn = styled.button`
+  background-color: red;
+  padding: .5rem 1rem;
+  color: white;
+  font-weight: 600;
+  font-size: 1.25rem;
+  border-radius: 10px;
+  margin-left: 50%;
+  margin-top: 10%;
+`
 
 export default StyledComponents;
